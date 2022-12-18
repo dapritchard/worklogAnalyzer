de334418dfc4975cee047a633b1f6ee75c3ce6d1
David Pritchard <david.al.pritchard@gmail.com>
2022-12-18 18:59:04 -0500

    change worklogs node structure

--- R/worklogs.R
+++ R/worklogs.R
@@ -8,12 +8,9 @@ worklogs <- function(x) {
 }
 
 mk_worklogs_node <- function(worklogs_node) {
-  assert_raw_worklogs_leaf(worklogs_node)
+  assert_raw_worklogs_node(worklogs_node)
   structure(
-    .Data = list(
-      name     = worklogs_node$name,
-      children = map(worklogs_node$children, worklogs)
-    ),
+    .Data = map(worklogs_node, worklogs),
     class = "worklogs_node"
   )
 }
@@ -28,12 +25,8 @@ mk_worklogs_leaf <- function(worklogs_leaf) {
 
 assert_raw_worklogs_node <- function(x) {
   stopifnot(
-    is.list(worklogs_node),
-    ! is.null(names(x)),
-    "name" %in% names(x),
-    check_string(x$name),
-    "children" %in% names(x),
-    is.list(x$children)
+    is.list(x),
+    ! is.null(names(x))
   )
 }
 
@@ -44,7 +37,17 @@ assert_raw_worklogs_leaf <- function(x) {
 }
 
 check_string <- function(x) {
-  is.character(x),
-  length(x) == 1L,
-  ! is.na(x)
+  stopifnot(
+    is.character(x),
+    length(x) == 1L,
+    ! is.na(x)
+  )
+}
+
+is_worklogs_node <- function(x) {
+  inherits(x, "worklogs_node")
+}
+
+is_worklogs_leaf <- function(x) {
+  inherits(x, "worklogs_leaf")
 }
