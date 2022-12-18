cd315ae2d78cc27ddb852402365c1392aab275a3
David Pritchard <david.al.pritchard@gmail.com>
2022-12-17 19:55:54 -0500

    add 'name' to 'worklogs_node'

--- R/worklogs.R
+++ R/worklogs.R
@@ -7,6 +7,17 @@ worklogs <- function(x) {
   )
 }
 
+mk_worklogs_node <- function(worklogs_node) {
+  assert_raw_worklogs_leaf(worklogs_node)
+  structure(
+    .Data = list(
+      name     = worklogs_node$name,
+      children = map(worklogs_node$children, worklogs)
+    ),
+    class = "worklogs_node"
+  )
+}
+
 mk_worklogs_leaf <- function(worklogs_leaf) {
   stopifnot(is.data.frame(worklogs_leaf))
   structure(
@@ -15,10 +26,25 @@ mk_worklogs_leaf <- function(worklogs_leaf) {
   )
 }
 
-mk_worklogs_node <- function(worklogs_node) {
-  stopifnot(is.list(worklogs_node))
-  structure(
-    .Data = map(worklogs_node, worklogs),
-    class = "worklogs_node"
+assert_raw_worklogs_node <- function(x) {
+  stopifnot(
+    is.list(worklogs_node),
+    ! is.null(names(x)),
+    "name" %in% names(x),
+    check_string(x$name),
+    "children" %in% names(x),
+    is.list(x$children)
   )
+}
+
+assert_raw_worklogs_leaf <- function(x) {
+  stopifnot(
+    is.data.frame(x)
+  )
+}
+
+check_string <- function(x) {
+  is.character(x),
+  length(x) == 1L,
+  ! is.na(x)
 }
