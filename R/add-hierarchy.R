7a164474277b3261850976747fc51990b718b0da
David Pritchard <david.al.pritchard@gmail.com>
2022-12-18 22:31:46 -0500

    add 'hierarchy_from_elements'

--- R/add-hierarchy.R
+++ R/add-hierarchy.R
@@ -58,3 +58,28 @@ add_hierarchy_leaf_char <- function(worklogs_leaf, hierarchy) {
 add_hierarchy_leaf_list <- function(worklogs_leaf, hierarchy) {
   stop("add_hierarchy_leaf_list is not yet implemented")
 }
+
+hierarchy_from_elements <- function(worklogs_leaf) {
+  split_children <- function(worklogs_leaf) {
+    parent <- map_chr(worklogs_leaf$parents, chuck, 1L)
+    worklogs_leaf$parents <- map(worklogs_leaf$parents, tail, n = -1L)
+    children <- add_hierarchy(worklogs_leaf, parent)
+    map(children, hierarchy_from_elements)
+  }
+  stopifnot(
+    is_worklogs_leaf(worklogs_leaf),
+    "parents" %in% names(worklogs_leaf),
+    is.list(worklogs_leaf$parents),
+    map_lgl(worklogs_leaf$parents, is.character)
+  )
+  n_no_parents <- sum(map_int(worklogs_leaf$parents, length) == 0L)
+  if ((n_no_parents != 0L) && (n_no_parents < nrow(worklogs_leaf))) {
+    print(worklogs_leaf$parents)
+    stop("Either none or all worklogs must have a parent")
+  }
+  `if`(
+    n_no_parents == 0L,
+    split_children(worklogs_leaf),
+    select(worklogs_leaf, -parents)
+  )
+}
