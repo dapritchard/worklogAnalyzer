0e081c81fd244c56234b3ed461c9ecf41108febe
David Pritchard <david.al.pritchard@gmail.com>
2022-12-20 13:05:49 -0500

    update 'worklogs' constructor

--- R/worklogs.R
+++ R/worklogs.R
@@ -1,10 +1,14 @@
 worklogs <- function(x) {
-  stopifnot(is.list(x) || is.data.frame(x))
-  `if`(
-    is.data.frame(x),
-    mk_worklogs_leaf(x),
-    mk_worklogs_node(x)
-  )
+  stopifnot(is.list(x))
+  if (is_worklogs(x)) {
+    return(x)
+  }
+  else if (is.data.frame(x)) {
+    return(mk_worklogs_leafs(x))
+  }
+  else {
+    return(mk_worklogs_node(x))
+  }
 }
 
 mk_worklogs_node <- function(worklogs_node) {
@@ -15,11 +19,19 @@ mk_worklogs_node <- function(worklogs_node) {
   )
 }
 
-mk_worklogs_leaf <- function(worklogs_leaf) {
+mk_worklogs_leafs <- function(worklogs_leaf) {
+  add_class_info <- function(raw_leaf) {
+    structure(
+      .Data = raw_leaf,
+      class = c("worklogs_leaf", class(worklogs_leaf))
+    )
+  }
   stopifnot(is.data.frame(worklogs_leaf))
+  worklogs_leaf_tibble <- as_tibble(worklogs_leaf)
+  raw_leafs <- split(worklogs_leaf_tibble, worklogs_leaf_tibble$task)
   structure(
-    .Data = worklogs_leaf,
-    class = c("worklogs_leaf", class(worklogs_leaf))
+    .Data = map(raw_leafs, add_class_info),
+    class = "worklogs_node"
   )
 }
 
@@ -36,12 +48,16 @@ assert_raw_worklogs_leaf <- function(x) {
   )
 }
 
-check_string <- function(x) {
+assert_string <- function(x) {
   stopifnot(
     is.character(x),
     length(x) == 1L,
     ! is.na(x)
   )
+}
+
+is_worklogs <- function(x) {
+  is_worklogs_node(x) || is_worklogs_leaf(x)
 }
 
 is_worklogs_node <- function(x) {
