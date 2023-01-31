b51265514b36182aca1113da828e0618917efe28
David Pritchard <david.al.pritchard@gmail.com>
2023-01-30 22:12:42 -0500

    rework breakpoints and partitioning

--- R/partition-efforts.R
+++ R/partition-efforts.R
@@ -1,63 +1,72 @@
-partition_by_intervals <- function(worklogs,
+partition_by_intervals <- function(worklogs_df,
+                                   start_time,
                                    period,
-                                   start,
                                    directions,
                                    config) {
+  filter_worklogs_by_time <- function(before_time, after_time) {
+    keep_lgl <- (before_time <= worklogs_start) & (worklogs_start < after_time)
+    worklogs_df[keep_lgl, ]
+  }
   stopifnot(
+    # TODO: other assertions
     directions %in% c("before", "after", "both")
   )
-
-  is_before <- directions %in% c("before", "both")
-  is_after <- directions %in% c("after", "both")
-
-  worklogs_start <- worklogs[[config@labels@start]]
-  earliest_start <- min(worklogs_start)
-  latest_start <- max(worklogs_start)
-
-  if (is_before) {
-    before_dfs <- list()
-  }
-  else {
-    before_dfs <- list()
-  }
-
-  if (is_after) {
-    after_dfs <- list()
-  }
-  else {
-    after_dfs <- list()
-  }
-
-  c(before_dfs, after_dfs)
+  worklogs_start <- worklogs_df[[config@labels@start]]
+  breakpoints <- calc_breakpoints(
+    worklogs_start,
+    start_time,
+    period, directionsi
+  )
+  breakpoint_pairs <- tibble(
+    before_time = head(breakpoints, -1),
+    after_time  = breakpoints[-1]
+  )
+  pmap(breakpoint_pairs, filter_worklogs_by_time)
 }
 
-# calc_breakpoints_before <- function(earliest_start, start, period) {
-#   n_periods <- 0L
-#   current_start
-#   while (earliest_start <= start) {
+calc_breakpoints <- function(worklogs_start,
+                             start_time,
+                             period,
+                             directions) {
+  breakpoints_before <- `if`(
+    directions %in% c("before", "both"),
+    calc_breakpoints_before(min(worklogs_start), start_time, period),
+    parse_date_time(character(0L))
+  )
+  breakpoints_after <- `if`(
+    directions %in% c("after", "both"),
+    calc_breakpoints_after(start_time, max(worklogs_start), period),
+    start_time
+  )
+  c(breakpoints_before, breakpoints_after)
+}
 
-#   }
-# }
-
-calc_breakpoints_before <- function(time_before, time_after, period) {
-  if (time_before >= time_after) {
-    return(time_after)
+calc_breakpoints_before <- function(min_time, start_time, period) {
+  if (min_time >= start_time) {
+    return(parse_date_time(character(0L)))
   }
-  interval_val <- interval(time_before, time_after)
+  interval_val <- interval(min_time, start_time)
   n_periods <- ceiling(interval_val / period)
-  sort(time_after - ((0 : n_periods) * period))
+  sort(start_time - (seq(1, n_periods, 1) * period))
 }
 
-calc_breakpoints_after <- function(time_before, time_after, period) {
-  if (time_before > time_after) {
-    return(time_before)
+calc_breakpoints_after <- function(start_time, time_after, period) {
+  if (start_time > time_after) {
+    return(start_time)
   }
-  interval_val <- interval(time_before, time_after)
+  interval_val <- interval(start_time, time_after)
   n_periods_orig <- interval_val / period
   n_periods <- `if`(
     floor(n_periods_orig) == n_periods_orig,
     n_periods_orig + 1,
     ceiling(n_periods_orig)
   )
-  time_before + ((0 : n_periods) * period)
+  start_time + ((0 : n_periods) * period)
 }
+
+calc_breakpoints(
+  parse_date_time(c("20220101", "20220103", "20220105"), "ymd"),
+  parse_date_time("2022010406", "ymdh"),
+  hours(8),
+  "both"
+)
