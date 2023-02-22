4852549fa66460c0dfdb8abed4995e9777026fc3
David Pritchard <david.al.pritchard@gmail.com>
2023-02-21 23:06:03 -0500

    remove unused 'subtree_extract'

--- R/subtree.R
+++ R/subtree.R
@@ -1,45 +1,44 @@
-# TODO: make this an S4 method?
-subtree_extract <- function(worklogs_node, path) {
-  stopifnot(
-    is(worklogs_node, "worklogs_node"),
-    is_chr_nomiss(path)
-  )
-  if (length(path) == 0L) {
-    return(worklogs_node)
-  }
-  else if (length(path) == 1L) {
-    child <- chuck(worklogs_node, path[1L])
-    if (is(child, "worklogs_node")) {
-      return(child)
-    }
-    else if (is(child, "worklogs_leaf")) {
-      return(worklogs_node[path[1L]])
-    }
-    else {
-      msg <- sprintf(
-        "Invalid input data. The %s child is not a worklogs type",
-        path[1L]
-      )
-      stop(msg, call. = FALSE)
-    }
-  }
-  else {
-    child <- chuck(worklogs_node, path[1L])
-    if (is(child, "worklogs_node")) {
-      return(subtree_extract(child, path[-1L]))
-    }
-    else if (is(child, "worklogs_leaf")) {
-      stop("Cannot take the child of a leaf", call. = FALSE)
-    }
-    else {
-      msg <- sprintf(
-        "Invalid input data. The %s child is not a worklogs type",
-        path[1L]
-      )
-      stop(msg, call. = FALSE)
-    }
-  }
-}
+# subtree_extract <- function(worklogs_node, path) {
+#   stopifnot(
+#     is(worklogs_node, "worklogs_node"),
+#     is_chr_nomiss(path)
+#   )
+#   if (length(path) == 0L) {
+#     return(worklogs_node)
+#   }
+#   else if (length(path) == 1L) {
+#     child <- chuck(worklogs_node, path[1L])
+#     if (is(child, "worklogs_node")) {
+#       return(child)
+#     }
+#     else if (is(child, "worklogs_leaf")) {
+#       return(worklogs_node[path[1L]])
+#     }
+#     else {
+#       msg <- sprintf(
+#         "Invalid input data. The %s child is not a worklogs type",
+#         path[1L]
+#       )
+#       stop(msg, call. = FALSE)
+#     }
+#   }
+#   else {
+#     child <- chuck(worklogs_node, path[1L])
+#     if (is(child, "worklogs_node")) {
+#       return(subtree_extract(child, path[-1L]))
+#     }
+#     else if (is(child, "worklogs_leaf")) {
+#       stop("Cannot take the child of a leaf", call. = FALSE)
+#     }
+#     else {
+#       msg <- sprintf(
+#         "Invalid input data. The %s child is not a worklogs type",
+#         path[1L]
+#       )
+#       stop(msg, call. = FALSE)
+#     }
+#   }
+# }
 
 subtree_remove <- function(worklogs_node, path) {
   stopifnot(
